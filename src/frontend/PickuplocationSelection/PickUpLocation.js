f34635d2eb60865129b7abb77356299e98305d69
Sabareesh Subramanian <sabareesh16797@gmail.com>
2021-08-27 17:36:25 +0530

    Made popular points, airport data dynamic based on city selected

--- src/frontend/PickuplocationSelection/PickUpLocation.js
+++ src/frontend/PickuplocationSelection/PickUpLocation.js
@@ -1,150 +1,133 @@
+let search = document.getElementById("searches");
+search.placeholder = `Tell us your Starting point in ${localStorage.getItem(
+  "SelectedCity"
+)}`;
+localStorage.setItem("address", "");
+console.log(localStorage.getItem("address"));
+
+let airporttext = document.getElementById("airport_text");
+airporttext.innerText = `${localStorage.getItem("SelectedCity")} Airport`;
+
 //Live location
 var label = document.getElementById("labelled");
 function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-    } else {
-        alert("Cannot find Location");
-    }
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    alert("Cannot find Location");
+  }
 }
 function showPosition(position) {
-    var str = "Latitude: " + position.coords.latitude +
-        "<br>Longitude: " + position.coords.longitude;
-    console.log(str);
+  var str =
+    "Latitude: " +
+    position.coords.latitude +
+    "<br>Longitude: " +
+    position.coords.longitude;
+  console.log(str);
 }
 label.addEventListener("click", getLocation);
 
 //overlay
 function on() {
-    document.getElementById("overlay").style.display = "block";
+  document.getElementById("overlay").style.display = "block";
 }
 
 function off() {
-    document.getElementById("overlay").style.display = "none";
+  document.getElementById("overlay").style.display = "none";
 }
 
 //multiple cities select
 var div = document.getElementById("choosed");
 var selectedCity = document.getElementById("selectedCity");
 function cities() {
-    selectedCity.textContent = null;
-    div.innerHTML = '<input type="text" placeholder="Enter city" autofocus="autofocus"><span></span><ul><li>Ahmedabad</li><li>Bangalore</li><li>Bhopal</li><li>Bhubaneswar</li><li>Calicut</li><li>Chandigarh</li><li>Chennai</li><li>Coimbatore</li><li>Delhi NCR</li><li>Guwahati</li><li>Hyderabad</li><li>Indore</li><li>Jaipur</li><li>Kochi</li><li>Kolkata</li><li>Lucknow</li><li>Mumbai</li><li>Mysore</li><li>Nagpur</li><li>Nashik</li><li>Patna</li><li>Pune</li><li>Raipur</li><li>Siliguri</li><li>Trichy</li><li>Udupi-Manipal</li><li>Vadodara</li><li>Vijayawada</li><li>Vizag</li></ul>';
+  selectedCity.textContent = null;
+  div.innerHTML =
+    '<input type="text" placeholder="Enter city" autofocus="autofocus"><span></span><ul><li>Ahmedabad</li><li>Bangalore</li><li>Bhopal</li><li>Bhubaneswar</li><li>Calicut</li><li>Chandigarh</li><li>Chennai</li><li>Coimbatore</li><li>Delhi NCR</li><li>Guwahati</li><li>Hyderabad</li><li>Indore</li><li>Jaipur</li><li>Kochi</li><li>Kolkata</li><li>Lucknow</li><li>Mumbai</li><li>Mysore</li><li>Nagpur</li><li>Nashik</li><li>Patna</li><li>Pune</li><li>Raipur</li><li>Siliguri</li><li>Trichy</li><li>Udupi-Manipal</li><li>Vadodara</li><li>Vijayawada</li><li>Vizag</li></ul>';
 }
 
 var list = document.querySelector("input");
 function trigger() {
-    selectedCity.textContent = "Mumbai";
-    div.innerHTML = '<div id="selectedCity">Mumbai</div> <span></span>';
+  selectedCity.textContent = "Mumbai";
+  div.innerHTML = '<div id="selectedCity">Mumbai</div> <span></span>';
 }
 list.addEventListener("click", trigger);
 
-//Square box selectors
 var addressDisplayDiv = document.getElementById("addressDisplay");
-function setBox1() {
-    let item1 = document.getElementById("item1");
-    let item2 = document.getElementById("item2");
-    let item3 = document.getElementById("item3");
-    let item4 = document.getElementById("terminal1");
-    let item5 = document.getElementById("terminal2");
-    item1.style.border = "2px solid rgb(112, 173, 77)";
-    item1.style.boxShadow = "0 4px 12px rgb(0 0 0 / 18%)";
-    item2.style.border = "0px solid rgb(50, 58, 68)";
-    item2.style.boxShadow = "none";
-    item3.style.border = "0px solid rgb(50, 58, 68)";
-    item3.style.boxShadow = "none";
-    item4.style.border = "1px solid rgb(50, 58, 68)";
-    item4.style.boxShadow = "none";
-    item5.style.border = "1px solid rgb(50, 58, 68)";
-    item5.style.boxShadow = "none";
-    localStorage.setItem("address", "Kandivali West");
-    addressDisplayDiv.innerHTML = `${localStorage.getItem("address")}`;
-    document.getElementById("searches").value = `${localStorage.getItem("address")}`;
-}
-function setBox2() {
-    let item1 = document.getElementById("item1");
-    let item2 = document.getElementById("item2");
-    let item3 = document.getElementById("item3");
-    let item4 = document.getElementById("terminal1");
-    let item5 = document.getElementById("terminal2");
-    item2.style.border = "2px solid rgb(112, 173, 77)";
-    item2.style.boxShadow = "0 4px 12px rgb(0 0 0 / 18%)";
-    item1.style.border = "0px solid rgb(50, 58, 68)";
-    item1.style.boxShadow = "none";
-    item3.style.border = "0px solid rgb(50, 58, 68)";
-    item3.style.boxShadow = "none";
-    item4.style.border = "1px solid rgb(50, 58, 68)";
-    item4.style.boxShadow = "none";
-    item5.style.border = "1px solid rgb(50, 58, 68)";
-    item5.style.boxShadow = "none";
-    localStorage.setItem("address", "Thane");
-    addressDisplayDiv.innerHTML = `${localStorage.getItem("address")}`;
-    document.getElementById("searches").value = `${localStorage.getItem("address")}`;
-}
-function setBox3() {
-    let item1 = document.getElementById("item1");
-    let item2 = document.getElementById("item2");
-    let item3 = document.getElementById("item3");
-    let item4 = document.getElementById("terminal1");
-    let item5 = document.getElementById("terminal2");
-    item3.style.border = "2px solid rgb(112, 173, 77)";
-    item3.style.boxShadow = "0 4px 12px rgb(0 0 0 / 18%)";
-    item2.style.border = "0px solid rgb(50, 58, 68)";
-    item2.style.boxShadow = "none";
-    item1.style.border = "0px solid rgb(50, 58, 68)";
-    item1.style.boxShadow = "none";
-    item4.style.border = "1px solid rgb(50, 58, 68)";
-    item4.style.boxShadow = "none";
-    item5.style.border = "1px solid rgb(50, 58, 68)";
-    item5.style.boxShadow = "none";
-    localStorage.setItem("address", "Kharghar");
-    addressDisplayDiv.innerHTML = `${localStorage.getItem("address")}`;
-    document.getElementById("searches").value = `${localStorage.getItem("address")}`;
-}
-function setBox4() {
-    let item1 = document.getElementById("item1");
-    let item2 = document.getElementById("item2");
-    let item3 = document.getElementById("item3");
-    let item4 = document.getElementById("terminal1");
-    let item5 = document.getElementById("terminal2");
-    item4.style.border = "2px solid rgb(112, 173, 77)";
-    item4.style.boxShadow = "0 4px 12px rgb(0 0 0 / 18%)";
-    item2.style.border = "0px solid rgb(50, 58, 68)";
-    item2.style.boxShadow = "none";
-    item3.style.border = "0px solid rgb(50, 58, 68)";
-    item3.style.boxShadow = "none";
-    item1.style.border = "0px solid rgb(50, 58, 68)";
-    item1.style.boxShadow = "none";
-    item5.style.border = "1px solid rgb(50, 58, 68)";
-    item5.style.boxShadow = "none";
-    localStorage.setItem("address", "International Terminal (T2)");
-    addressDisplayDiv.innerHTML = `${localStorage.getItem("address")}`;
-    document.getElementById("searches").value = `${localStorage.getItem("address")}`;
-}
-function setBox5() {
-    let item1 = document.getElementById("item1");
-    let item2 = document.getElementById("item2");
-    let item3 = document.getElementById("item3");
-    let item4 = document.getElementById("terminal1");
-    let item5 = document.getElementById("terminal2");
-    item5.style.border = "2px solid rgb(112, 173, 77)";
-    item5.style.boxShadow = "0 4px 12px rgb(0 0 0 / 18%)";
-    item2.style.border = "0px solid rgb(50, 58, 68)";
-    item2.style.boxShadow = "none";
-    item3.style.border = "0px solid rgb(50, 58, 68)";
-    item3.style.boxShadow = "none";
-    item4.style.border = "1px solid rgb(50, 58, 68)";
-    item4.style.boxShadow = "none";
-    item1.style.border = "0px solid rgb(50, 58, 68)";
-    item1.style.boxShadow = "none";
-    localStorage.setItem("address", "Domestic Terminal (T1A/T1B)");
-    addressDisplayDiv.innerHTML = `${localStorage.getItem("address")}`;
-    document.getElementById("searches").value = `${localStorage.getItem("address")}`;
-}
 
 //next button function
-var proceeding = document.getElementById("proceeding");
-function onProceed() {
-    var data = document.getElementById("searches").value;
-    localStorage.setItem("address", `${data}`)
+// var proceeding = document.getElementById("proceeding");
+// function onProceed() {
+//   var data = document.getElementById("searches").value;
+//   localStorage.setItem("address", `${data}`);
+// }
+// proceeding.addEventListener("click", onProceed);
+
+// Adding popular pick points
+async function addPopularPlaces() {
+  let popularOld = await fetch(
+    `http://localhost:4321/popularpoints/${localStorage.getItem(
+      "SelectedCity"
+    )}`
+  );
+  let citydetails = await popularOld.json();
+  citydetails = citydetails[0];
+  //   console.log("citydetails:", citydetails);
+  let popularpoints = document.getElementById("popularpoints");
+  for (place of citydetails.popular_places) {
+    let p = document.createElement("div");
+    p.setAttribute("class", "items");
+    p.innerHTML = place;
+    p.addEventListener("click", () => {
+      let divs = document.querySelectorAll("div");
+      for (let div of divs) {
+        div.style.border = "0px";
+      }
+      p.style.border = "2px solid #70ad4d";
+      localStorage.setItem("address", p.innerHTML);
+      addressDisplayDiv.innerHTML = `${localStorage.getItem("address")}`;
+      document.getElementById("searches").value = `${localStorage.getItem(
+        "address"
+      )}`;
+    });
+    popularpoints.append(p);
+  }
+  let terminallist = document.getElementById("terminal_names");
+  for (airport of citydetails.airports) {
+    let p = document.createElement("button");
+    p.setAttribute("class", "terminal");
+    p.innerHTML = airport;
+    p.addEventListener("click", () => {
+      let divs = document.querySelectorAll("button");
+      for (let div of divs) {
+        div.style.border = "0px";
+      }
+      p.style.border = "2px solid #70ad4d";
+      localStorage.setItem("address", p.innerHTML);
+      addressDisplayDiv.innerHTML = `${localStorage.getItem("address")}`;
+      document.getElementById("searches").value = `${localStorage.getItem(
+        "address"
+      )}`;
+    });
+    terminallist.append(p);
+  }
 }
-proceeding.addEventListener("click", onProceed);
+
+let proceeding = document.getElementById("nextbtn");
+
+let checkNext = setInterval(() => {
+  let address = localStorage.getItem("address");
+  console.log("address:", address);
+  if (address == "") {
+    proceeding.disabled = true;
+    proceeding.style.cursor = "default";
+    proceeding.style.backgroundColor = "grey";
+  } else {
+    proceeding.style.cursor = "pointer";
+    proceeding.style.backgroundColor = "#70ad4d";
+    clearInterval(checkNext);
+  }
+}, 100);
+
+addPopularPlaces();
+
+// setInterval({},100)
--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const testController = require("./controllers/test.controller");
 const carsController = require("./controllers/car.controller");
 const checkoutController = require("./controllers/checkout.controller");
+const popularpointsController = require("./controllers/popularpoints.controller");
 const app = express();
 
 app.use(express.json());
@@ -13,6 +14,8 @@ app.use(cors());
 app.use("/cars", carsController);
 app.use(cors());
 app.use("/checkout", checkoutController);
+app.use(cors());
+app.use("/popularpoints", popularpointsController);
 
 app.listen(4321, async () => {
   await connect();
